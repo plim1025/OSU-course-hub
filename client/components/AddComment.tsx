e2b53c85d2a6844ffee1abcd8709010ad532f401
weiyutang <weiyutang1010@outlook.com>
2021-01-08 11:43:11 +0800

    Include modal component

---
+++ client/components/AddComment.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const AddComment: React.FC<Props> = () => {
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<div>
+			<Button variant='primary' onClick={handleShow}>
+				Add Comment
+			</Button>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Modal heading</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+				<Modal.Footer>
+					<Button variant='secondary' onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant='primary' onClick={handleClose}>
+						Save Changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
+};
+
+export default AddComment;
